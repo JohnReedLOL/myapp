9909da96711e01d8cda479d62fd9fb10a0abe0e7
Ubuntu <ubuntu@ip-172-31-29-213.us-east-2.compute.internal>
2017-08-22 02:08:58 +0000

    Using Heroku port (https://stackoverflow.com/questions/7459166/can-i-have-node-js-listen-on-a-non-standard-port-when-hosted-on-heroku)

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(12345,"0.0.0.0");
+app.listen(process.env.PORT || 12345, "0.0.0.0");
+// app.listen(12345,"0.0.0.0");
 // app.listen(3000, function () {
 //   console.log('Example app listening on port 3000!');
 // });
