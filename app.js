dbabe5dafee70392c3c9a670f646f35936da488f
Ubuntu <ubuntu@ip-172-31-29-213.us-east-2.compute.internal>
2017-08-22 02:35:02 +0000

    CI test

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.get('/', function (req, res) {
 
 app.listen(process.env.PORT || 12345, "0.0.0.0");
 // app.listen(12345,"0.0.0.0");
+
 // app.listen(3000, function () {
 //   console.log('Example app listening on port 3000!');
 // });
